d1dae799cc2799619ea9c6b12ec31cf5f5f331a9
ManganiBiagio <biagiomangani@live.it>
2022-11-16 15:30:34 +0100

    intervall change

--- js/caroselObject.js
+++ js/caroselObject.js
@@ -31,66 +31,68 @@ const anteprimaImg = document.querySelector(".anteprima-box");
 //creo un indice per il carosello
 let indice = 0;
 //creo un array che conterra il riferimento agli elementi img da nascondere e mostrare al cambio indice
-const carosuelItemList=[];
+const carosuelItemList = [];
 
 //creo un array che conterra il riferimento alle anteprime img
-const anteprimeImgList=[];
+const anteprimeImgList = [];
 //creo un array che conterra il riferimento alle anteprime div
-const anteprimeDivList=[]
+const anteprimeDivList = []
+
+
 
 //Genero dinamicamente il contenuto del mio carosello
-for(let i =0;i<images.length;i++){
-    
-   
-    
+for (let i = 0; i < images.length; i++) {
+
+
+
     //creo un elemento div che conterra il boxTesto e l'elemento img
-    const boxEl=document.createElement("div");
+    const boxEl = document.createElement("div");
 
     //il mio elemetno lo salvo al interno dell'array dichiarato fuori dal for
     carosuelItemList.push(boxEl);
 
-    
+
     //elemento coorispondente al mio indice ("sarà sempre 0")
     if (i !== indice) {
         boxEl.classList.add("d-none");
     }
     //utilizzo la variabile precedentemente creata per nascondere o mostrare il mio elemento genitore
-    
+
 
     //creo gli elementi da appendere al genitore
 
     //creo un elemento img gli aggiungo classe e proprietà
     const imgEl = document.createElement("img");
     imgEl.classList.add("my-img");
-    imgEl.src=images[i].image;
+    imgEl.src = images[i].image;
 
     //creo un elemento div che conterra il mio titolo e sottotitolo
-    const boxTxtEl=document.createElement("div");
+    const boxTxtEl = document.createElement("div");
     boxTxtEl.classList.add("txt-img");
 
     //creo elemento h1 e h5 come titolo e sottotitolo e gòi assegno il valore da array di oggetti
-    const h1El=document.createElement("h1");
-    const subtittle=document.createElement("h5");
-    h1El.innerText=images[i].title;
-    subtittle.innerText=images[i].text;
+    const h1El = document.createElement("h1");
+    const subtittle = document.createElement("h5");
+    h1El.innerText = images[i].title;
+    subtittle.innerText = images[i].text;
 
     //appendo gli elementi ai corrispettivi genitori
-    boxTxtEl.append(h1El,subtittle);
-    boxEl.append(boxTxtEl,imgEl);
+    boxTxtEl.append(h1El, subtittle);
+    boxEl.append(boxTxtEl, imgEl);
     boxImagesEl.append(boxEl)
 
 
     //creo due elementi uno per l'effetto e l'altro contenitore delle immagini
     //e aggiungo rispettive classi
-    const divEffect=document.createElement("div");
-    const container=document.createElement("div");
+    const divEffect = document.createElement("div");
+    const container = document.createElement("div");
     container.classList.add("relative");
     divEffect.classList.add("img-box");
-    
+
     //creo un elemento img aggiunggo src e classe
     const anteprimaEl = document.createElement("img");
     anteprimaEl.src = images[i].image;
-    anteprimaEl.classList.add( "anteprima-img");
+    anteprimaEl.classList.add("anteprima-img");
     anteprimaEl.alt = "";
     //attivo l'immagine con l'effetto corrispondente all'indice
     if (i === indice) {
@@ -114,51 +116,89 @@ for(let i =0;i<images.length;i++){
 
 
 
+
 }
+setInterval(pushDown, 3000);
 
-btnDownEl.addEventListener("click",function(){
+// btnDownEl.addEventListener("click", function () {
 
+//     //incremento l'indice
+//     indice++;
+
+//     //mi salvo in una const la qunatità di oggetti che ho
+//     const n = carosuelItemList.length;
+//     changeCarosuel(indice, n, false)
+
+//     // //(indice-1) % n + n) % n utilizzo questa formula per creare un array circolare
+//     // //cosi facendo l'indice sarà sempre in range
+//     // //infine all'indice selezionato aggiungo o tolgo d-none
+//     // carosuelItemList[((indice-1) % n + n) % n].classList.toggle("d-none");
+//     // carosuelItemList[(indice % n + n) % n].classList.toggle("d-none");  
+
+//     // //aggiungo e tolgo al thumbnail gli effetti
+//     // anteprimeImgList[((indice-1) % n + n) % n].classList.toggle("border-blue");
+//     // anteprimeImgList[((indice) % n + n) % n].classList.toggle("border-blue");
+//     // anteprimeDivList[((indice-1) % n + n) % n].classList.toggle("effetto-anteprima");
+//     // anteprimeDivList[((indice) % n + n) % n].classList.toggle("effetto-anteprima");
+
+
+
+// });
+
+btnDownEl.addEventListener("click",pushDown)
+function pushDown() {
     //incremento l'indice
     indice++;
-
     //mi salvo in una const la qunatità di oggetti che ho
-    const n=carosuelItemList.length;
+    const n = carosuelItemList.length;
+    changeCarosuel(indice, n, false)
 
-    //(indice-1) % n + n) % n utilizzo questa formula per creare un array circolare
-    //cosi facendo l'indice sarà sempre in range
-    //infine all'indice selezionato aggiungo o tolgo d-none
-    carosuelItemList[((indice-1) % n + n) % n].classList.toggle("d-none");
-    carosuelItemList[(indice % n + n) % n].classList.toggle("d-none");  
-    
-    //aggiungo e tolgo al thumbnail gli effetti
-    anteprimeImgList[((indice-1) % n + n) % n].classList.toggle("border-blue");
-    anteprimeImgList[((indice) % n + n) % n].classList.toggle("border-blue");
-    anteprimeDivList[((indice-1) % n + n) % n].classList.toggle("effetto-anteprima");
-    anteprimeDivList[((indice) % n + n) % n].classList.toggle("effetto-anteprima");
+}
 
-    
-
-});
-
-btnUpEl.addEventListener("click",function(){
+btnUpEl.addEventListener("click", function () {
 
     //decremento l'indice
     indice--;
 
     //mi salvo in una const la qunatità di oggetti che ho
-    const n=carosuelItemList.length;
+    const n = carosuelItemList.length;
 
-    //(indice-1) % n + n) % n utilizzo questa formula per creare un array circolare
-    //cosi facendo l'indice sarà sempre in range
-    //infine all'indice selezionato aggiungo o tolgo d-none
-    carosuelItemList[((indice+1) % n + n) % n].classList.toggle("d-none");
-    carosuelItemList[(indice % n + n) % n].classList.toggle("d-none");   
+    changeCarosuel(indice, n, true)
+    // //(indice-1) % n + n) % n utilizzo questa formula per creare un array circolare
+    // //cosi facendo l'indice sarà sempre in range
+    // //infine all'indice selezionato aggiungo o tolgo d-none
+    // carosuelItemList[((indice+1) % n + n) % n].classList.toggle("d-none");
+    // carosuelItemList[(indice % n + n) % n].classList.toggle("d-none");   
 
-    //aggiungo e tolgo al thumbnail gli effetti
-    anteprimeImgList[((indice+1) % n + n) % n].classList.toggle("border-blue");
-    anteprimeImgList[((indice) % n + n) % n].classList.toggle("border-blue");
-    anteprimeDivList[((indice+1) % n + n) % n].classList.toggle("effetto-anteprima");
-    anteprimeDivList[((indice) % n + n) % n].classList.toggle("effetto-anteprima");
+    // //aggiungo e tolgo al thumbnail gli effetti
+    // anteprimeImgList[((indice+1) % n + n) % n].classList.toggle("border-blue");
+    // anteprimeImgList[((indice) % n + n) % n].classList.toggle("border-blue");
+    // anteprimeDivList[((indice+1) % n + n) % n].classList.toggle("effetto-anteprima");
+    // anteprimeDivList[((indice) % n + n) % n].classList.toggle("effetto-anteprima");
 });
 
+/**
+ * Questa funzione cambia le classi per generare l'effetto carosuel gli passo come valori 
+ * indice del lettore la lnghezza degli elementi e un booleano per capire se il bottone 
+ * premuto e up or down
+ * 
+ * @param {number} indice  
+ * @param {number} lenght 
+ * @param {boolean} isUp 
+ */
+function changeCarosuel(indice, lenght, isUp) {
+    let k = -1;
+    if (isUp) {
+        k = 1;
+    }
 
+    carosuelItemList[((indice + k) % lenght + lenght) % lenght].classList.toggle("d-none");
+    carosuelItemList[(indice % lenght + lenght) % lenght].classList.toggle("d-none");
+
+    //aggiungo e tolgo al thumbnail gli effetti
+    anteprimeImgList[((indice + k) % lenght + lenght) % lenght].classList.toggle("border-blue");
+    anteprimeImgList[((indice) % lenght + lenght) % lenght].classList.toggle("border-blue");
+    anteprimeDivList[((indice + k) % lenght + lenght) % lenght].classList.toggle("effetto-anteprima");
+    anteprimeDivList[((indice) % lenght + lenght) % lenght].classList.toggle("effetto-anteprima");
+
+}
